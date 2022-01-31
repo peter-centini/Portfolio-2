05890418323ebda4417b4061e5bf6b08dd145f52
Peter <peter@MacBook-Air-de-Peter.local>
2022-01-31 20:42:17 +0100

    finished portfolio-2

---
+++ src/components/about/About.jsx
@@ -0,0 +1,44 @@
+import "./about.css";
+import Award from "../../asset/award.png";
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/03/05/22/53/camera-1239384_960_720.jpg"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">À propos de Moi</h1>
+        <p className="a-sub">
+          C'est un fait établi depuis longtemps qu'un lecteur sera distrait par le
+          contenu lisible.
+        </p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+        <div className="a-award">
+          <img src={Award} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-award-title">Internationale Design Awards 2021</h4>
+            <p className="a-award-desc">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
+              and fugit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,75 @@
+import "./contact.css";
+import Phone from "../../asset/phone.png";
+import Email from "../../asset/email.png";
+import Address from "../../asset/address.png";
+import { useContext, useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import { ThemeContext } from "../../context";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false)
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_x0sqggi",
+        "template_u9ywnh5",
+        formRef.current,
+        "user_kqRL6JF5N8VXvL0nfvVtm"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Discutons de votre projet</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +33 698109896
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              contact@Peter.centini.dev
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              175 Rue de Paris, Paris  75000 Françe
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>Quelle est votre histoire ?</b> Contactez-moi. Toujours disponible pour
+            freelance si le bon projet se présente à moi.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Votre Nom" name="user_name" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Sujet de contact" name="user_subject" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Ton Email" name="user_email" />
+            <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Ton Message" name="message" />
+            <button>Submit</button>
+            {done && "Thank you..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
